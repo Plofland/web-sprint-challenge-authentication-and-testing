7c4ae2fb4e8b04ec06e8a72e1ebceb6b232eb974
Plofland <ploflan@gmail.com>
2021-02-26 12:49:09 -0500

    Initial commit. API working and sanity test passing.

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,21 @@
-// Write your tests here
+const request = require('supertest');
+const server = require('./server');
+
 test('sanity', () => {
-  expect(true).toBe(false)
-})
+  expect(true).toBe(true);
+});
+
+const peter = { name: 'Peter' };
+
+describe('server', () => {
+  describe('server api working', () => {
+    it('responds with string -up & running-', async () => {
+      const res = await request(server).get('/');
+      expect(res.body.api).toBe('up & running');
+    });
+  });
+
+  // describe('register new user', () => {
+  //   it('')
+  // })
+});
